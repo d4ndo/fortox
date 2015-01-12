ba780493186fab08f5d4ce91ee2c2c52b66ddb70
Daniel Dörrhöfer <dando@gmx.de>
2015-01-12 13:59:29 +0100

    Added some quotes. Removed echo

--- fortox.sh
+++ fortox.sh
@@ -3,11 +3,11 @@
 reg="fortune";
 
 while true; do
-	inotifywait $1/text_out;
-	cmd=$(tail -n 1 $1/text_out);
+	inotifywait "$1/text_out";
+	cmd=$(tail -n 1 "$1/text_out");
 
-	if (( `echo $cmd | grep -c $reg` > 0 ))	then
-		echo $(fortune) > $1/text_in;
+ 	if grep "$reg" <<< "$cmd"; then
+		fortune > "$1/text_in";
 	fi
 done
  
