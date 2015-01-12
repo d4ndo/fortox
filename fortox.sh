cb734144c5d3b2d22861e458eec3095f853b7208
Daniel Dörrhöfer <dando@gmx.de>
2015-01-12 14:34:17 +0100

    keep grep quiet

--- fortox.sh
+++ fortox.sh
@@ -6,7 +6,7 @@ while true; do
 	inotifywait "$1/text_out";
 	cmd=$(tail -n 1 "$1/text_out");
 
- 	if grep "$reg" <<< "$cmd"; then
+ 	if grep -q "$reg" <<< "$cmd"; then
 		fortune > "$1/text_in";
 	fi
 done
