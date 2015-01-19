86ad7fe3472d5a0cc9257baa080ed9265441456c
Daniel Dörrhöfer <dando@gmx.de>
2015-01-19 15:47:17 +0100

    Changed var regf to reg.

--- fortox.sh
+++ fortox.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-reg="fortune";
+regf="(![Ff]ortune|![Ff])";
 
 while true; do
 	inotifywait "$1/text_out";
 	cmd=$(tail -n 1 "$1/text_out");
 
- 	if grep -q "$reg" <<< "$cmd"; then
+ 	if egrep -q "$regf" <<< "$cmd"; then
 		fortune > "$1/text_in";
 	fi
 done
