845c48a8259e46f9ff3798dafb4fd10df425e827
Daniel Dörrhöfer <dando@gmx.de>
2015-01-20 10:55:46 +0100

    Testing for ratox only. For the time being.

--- checkalive.sh
+++ checkalive.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#/bin/bash
 
 # add the following line in /etc/crontab
 # 0 *	* * *	root    cd /home/myname/fortox && ./checkalive.sh myprofile
@@ -7,14 +7,7 @@ if pgrep ratox; then
 	echo "ratox OK";
 else 
 	echo "restarting ratox";
-	ratox "$1";
-fi
-
-if pgrep fortox; then
-	echo "fortox OK";
-else 
-	echo "fortox restarting";
-	#fortox "$2";
+	ratox "$1" &bg;
 fi
 
 exit 0;
